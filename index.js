5dc74dacd9494305a1bdb46e5488a6393a196765
YASAR300 <everythingdecoding@gmail.com>
2024-11-20 14:54:53 +0530

    back

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const githubData = {
     "login": "YASAR300",
     "id": 179014663,
